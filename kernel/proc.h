ee5406ed8326d76df99a38e20c1998e50efae59d
leoliveirak <klitzkel789@gmail.com>
2025-05-28 17:19:20 -0300

    campo stride e pass em proc.h

--- kernel/proc.h
+++ kernel/proc.h
@@ -104,5 +104,7 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
-  int bilhete;
+  int bilhetes;
+  double stride;
+  double pass;
 };
